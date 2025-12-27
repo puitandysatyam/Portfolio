a8921873496ef17e28a852e8bf801faca1b6ab5f
Satyam Puitandy <satyam287b@gmail.com>
2025-12-27 12:54:26 +0530

    add skills

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,21 @@
   import Card from 'primevue/card';
   import ProgressBar from 'primevue/progressbar';
 
-  const skills = ref(["Spring Boot", "MongoDB", "Java"])
+  const skills = ref([
+    {name : "Spring Boot", proficiency : "45", },
+    {name : "MongoDB", proficiency : "70"},
+    {name : "Java", proficiency : "70"},
+    {name : "Python", proficiency : "75"},
+    {name : "JavaScript", proficiency : "30"},
+    {name : "Html & Css", proficiency : "50"},
+    {name : "Vue", proficiency : "40"},
+    {name : "C", proficiency : "60"},
+  ])
 </script>
 
 <template>
-  <section class="p-8"> <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+  <section class="p-8"> 
+    <div class="grid grid-cols-16 grid-rows-9 gap-4">
       <Card v-for="skill in skills" :key="skill">
         <template #title>{{ skill }}</template>
         <template #content>
