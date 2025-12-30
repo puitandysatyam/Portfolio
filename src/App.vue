de557efa23a001db83b688d27b4004e0acc75e4c
Satyam Puitandy <satyam287b@gmail.com>
2025-12-30 16:45:04 +0530

    added next bento box

--- src/App.vue
+++ src/App.vue
@@ -4,29 +4,52 @@
   import ProgressBar from 'primevue/progressbar';
 
   const skills = ref([
-    {name : "Spring Boot", proficiency : "45", },
-    {name : "MongoDB", proficiency : "70"},
-    {name : "Java", proficiency : "70"},
-    {name : "Python", proficiency : "75"},
-    {name : "JavaScript", proficiency : "30"},
-    {name : "Html & Css", proficiency : "50"},
-    {name : "Vue", proficiency : "40"},
-    {name : "C", proficiency : "60"},
+    {name : "Spring Boot", proficiency : 45, row : "row-span-6", col : "col-span-6"},
+    {name : "MongoDB", proficiency : 70, row : "row-span-1", col : "col-span-6"},
+    {name : "Java", proficiency : 70, row : "row-span-1", col : "col-span-6"},
+    {name : "Python", proficiency : 75, row : "row-span-1", col : "col-span-6"},
+    {name : "JavaScript", proficiency : 30, row : "row-span-1", col : "col-span-6"},
+    {name : "Html & Css", proficiency : 50, row : "row-span-1", col : "col-span-6"},
+    {name : "Vue", proficiency : 40, row : "row-span-1", col : "col-span-6"},
+    {name : "C", proficiency : 60, row : "row-span-1", col : "col-span-6"},
   ])
+  let sum =0
+  const avg_xp = ref(0)
+  for (let i =0; i < skills.value.length; i++){
+    
+    sum = skills.value[i].proficiency + sum
+  }
+  avg_xp.value = Math.round(sum/skills.value.length)
 </script>
 
 <template>
-  <section class="p-8"> 
-    <div class="grid grid-cols-16 grid-rows-9 gap-4">
-      <Card v-for="skill in skills" :key="skill">
-        <template #title>{{ skill }}</template>
-        <template #content>
-          <div class="mt-2">
-            <h5 class="text-sm mb-2 text-gray-600">Proficiency</h5>
-            <ProgressBar :value="70" :show-value="false" style="height: 6px;"/>
-          </div>
-        </template>
-      </Card>
+  <div class="grid grid-cols-12 grid-rows-6 gap-4 w-auto h-fill">
+    <div class="justify-center items-center col-span-7 row-span-2 border-2 border-green-400 rounded-2xl p-4">
+      <p>This is Satyam</p>
+      <div class="flex-1 p-4">
+        <p>Progress : </p>
+        <ProgressBar :value="avg_xp" />
+      </div>
+    </div> 
+    <div class="justify-center items-center col-span-5 row-span-4 border-2 border-green-400 rounded-2xl p-4">
+      <template v-for="skill in skills" :key="skill">
+        <Card>
+          <template #title>{{ skill.name }}</template>
+          <template #content>
+            <ProgressBar :value="skill.proficiency" />
+          </template>
+        </Card>
+      </template>
+
     </div>
-  </section>
+    <div>
+
+    </div>
+    <div>
+      
+    </div>
+    <div>
+      
+    </div>
+  </div>
 </template>
